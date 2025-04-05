bdfd1b2dd9477e088665b4de3ee26de4b2c19668
Alex Spencer <alexedwardspencer@gmail.com>
2025-04-05 19:40:03 +0100

    With translation

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct Model {
     border_size: f32,
     seed: u64,
     angle_noise: f32,
+    translation_limit: f32,
 }
 
 fn model(_app: &App) -> Model {
@@ -22,6 +23,7 @@ fn model(_app: &App) -> Model {
         border_size: 30.0,
         seed: 42,
         angle_noise: 0.5,
+        translation_limit: 15.0,
     }
 }
 
@@ -32,11 +34,6 @@ fn draw_row(draw: &Draw, area: &Rect, model: &Model, noise_limit: f32, rng: &mut
     let num_cubes = ((area.w() - model.border_size * 2.0) / model.cube_size).floor() as i32;
 
     for i in 0..num_cubes {
-        // Create rect
-        let rect = Rect::from_w_h(model.cube_size, model.cube_size)
-            .top_left_of(*area)
-            .shift_x(i as f32 * model.cube_size);
-
         // Determine angle to rotate. Use model's angle_noise param * noise_limit
         // to create a range of angles.
         let random_angle = if noise_limit > 0.0 {
@@ -46,6 +43,27 @@ fn draw_row(draw: &Draw, area: &Rect, model: &Model, noise_limit: f32, rng: &mut
         };
 
         // Shift the square up/down/left/right randomly
+        let x_shift = if noise_limit > 0.0 {
+            rng.gen_range(
+                (-model.translation_limit * noise_limit)..(model.translation_limit * noise_limit),
+            )
+        } else {
+            0.0
+        };
+        let y_shift = if noise_limit > 0.0 {
+            rng.gen_range(
+                (-model.translation_limit * noise_limit)..(model.translation_limit * noise_limit),
+            )
+        } else {
+            0.0
+        };
+
+        // Create rect
+        let rect = Rect::from_w_h(model.cube_size, model.cube_size)
+            .top_left_of(*area)
+            .shift_x(i as f32 * model.cube_size)
+            .shift_x(x_shift)
+            .shift_y(y_shift);
 
         // Draw rect
         draw.rect()
@@ -77,7 +95,7 @@ fn view(app: &App, model: &Model, frame: Frame) {
             &draw,
             &window.shift_y(-i as f32 * model.cube_size),
             model,
-            1.0 * (i as f32 / num_rows as f32),
+            i as f32 / num_rows as f32,
             &mut rng,
         );
     }
